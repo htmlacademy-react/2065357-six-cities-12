abeb88f4e9b545f16220ef95b60ebcd670a7683e
Vanesflat <ptptcptptc@mail.ru>
2023-03-11 00:20:01 +0700

    Add enums for routing

---
+++ project/src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Login = '/login',
+  Main = '/',
+  Offer = '/offer/:id',
+  Favorites = '/favorites',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
