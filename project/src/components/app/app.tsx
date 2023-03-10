677b12d54155efe14cb4ca01abf45213fcbb9b20
Vanesflat <ptptcptptc@mail.ru>
2023-03-11 00:24:07 +0700

    Realize routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import Favorites from '../../pages/Favorites/Favorites';
+import Login from '../../pages/Login/Login';
 import Main from '../../pages/Main/Main';
+import Offer from '../../pages/Offer/Offer';
+import Page404 from '../../pages/Page404/Page404';
+import Layout from '../Layout/Layout';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 type AppScreenProps = {
   placeCardCount: number;
@@ -6,7 +14,35 @@ type AppScreenProps = {
 
 function App({ placeCardCount }: AppScreenProps): JSX.Element {
   return (
-    <Main placeCardCount={placeCardCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<Layout />}>
+          <Route index element={<Main placeCardCount={placeCardCount} />} />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<Offer />}
+          />
+          <Route
+            path='*'
+            element={<Page404 />}
+          />
+        </Route>
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
