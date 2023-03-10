525efc6225c08fe70146b6083117dc7ea8cee7bc
Vanesflat <ptptcptptc@mail.ru>
2023-03-11 00:23:45 +0700

    Remove extra markup

--- project/src/pages/Page404/Page404.tsx
+++ project/src/pages/Page404/Page404.tsx
@@ -1,37 +1,9 @@
+import { Link } from 'react-router-dom';
 import classes from './Page404.module.scss';
 
 function Page404(): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href="/#">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
       <main className="page__main page__main--index page__main--index-empty">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -76,7 +48,7 @@ function Page404(): JSX.Element {
               <div className={classes['error__box']}>
                 <p className={classes['error__status']}>404</p>
                 <p className={classes['error__text']}>Page not found</p>
-                <a className={classes['error__link']} href="main.html">Back to main page</a>
+                <Link className={classes['error__link']} to='/'>Back to main page</Link>
               </div>
             </section>
             <div className="cities__right-section"></div>
