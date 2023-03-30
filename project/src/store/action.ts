e2ca85889af2bfa76a36e7b364b20561a44ae9b8
Vanesflat <ptptcptptc@yandex.ru>
2023-03-30 23:28:42 +0700

    Add new action for changing sort type

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,4 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
+import { SortType } from '../const';
 
 export const changeCity = createAction('changeCity', (city: string) => ({ payload: city }));
 export const addOffers = createAction('addOffers');
+export const changeSortType = createAction('changeSortType', (sortType: SortType) => ({ payload: sortType }));
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,14 +1,16 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { CITIES } from '../const';
+import { CITIES, SortType } from '../const';
 import { mockOffers } from '../mocks/offer';
 import { InitialState } from '../types/state';
-import { addOffers, changeCity } from './action';
+import { addOffers, changeCity, changeSortType } from './action';
 
 const DEFAULT_CITY = CITIES[0];
+const DEFAULT_SORT_TYPE = SortType.Popular;
 
 const initialState: InitialState = {
   city: DEFAULT_CITY,
-  offers: mockOffers
+  offers: mockOffers,
+  sortType: DEFAULT_SORT_TYPE
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -18,5 +20,8 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(addOffers, (state, action) => {
       state.offers = action.payload;
+    })
+    .addCase(changeSortType, (state, action) => {
+      state.sortType = action.payload;
     });
 });
