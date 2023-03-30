7afedb5e2f43ed2bb08f3632ef6e55cd6e1c8258
Vanesflat <ptptcptptc@yandex.ru>
2023-03-30 13:50:18 +0700

    Add function for get offers by current city

--- project/src/utils/common.ts
+++ project/src/utils/common.ts
@@ -1,3 +1,7 @@
+import { Offer } from '../types/offer';
+
 const ucFirst = (str: string): string => str[0].toUpperCase() + str.slice(1);
 
-export { ucFirst };
+const getOffersByCity = (city: string | undefined, offers: Offer[]): Offer[] => offers.filter((offer) => offer.city.name === city);
+
+export { ucFirst, getOffersByCity };
