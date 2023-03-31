d24b3e08e76d37695eb082997b82aac92fa7ca7d
Vanesflat <ptptcptptc@yandex.ru>
2023-03-31 12:53:50 +0700

    Implement outside click feature

--- project/src/components/sort/sort.tsx
+++ project/src/components/sort/sort.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import cn from 'classnames';
 import { SortType } from '../../const';
 import { useAppDispatch } from '../../hooks/use-app-dispatch/use-app-dispatch';
 import { changeSortType } from '../../store/action';
+import { useOnClickOutside } from '../../hooks/use-on-click-outside/use-on-click-outside';
 
 type SortProps = {
   currentSortType: SortType;
 }
 
-function Sort({currentSortType}: SortProps): JSX.Element {
+function Sort({ currentSortType }: SortProps): JSX.Element {
   const [isOpen, setIsOpen] = useState(false);
+  const sortRef = useRef(null);
 
   const dispatch = useAppDispatch();
 
+  useOnClickOutside(sortRef, () => setIsOpen(false));
+
   return (
     <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-caption">Sort by </span>
       <span
         className="places__sorting-type"
         tabIndex={0}
         onClick={() => setIsOpen(!isOpen)}
+        ref={sortRef}
       >
         {currentSortType}
         <svg className="places__sorting-arrow" width="7" height="4">
@@ -29,7 +34,7 @@ function Sort({currentSortType}: SortProps): JSX.Element {
       <ul className={cn('places__options places__options--custom', isOpen && 'places__options--opened')}>
         {Object.values(SortType).map((sortType) => (
           <li
-            className={cn('places__option', sortType === currentSortType && 'places__option--active')}
+            className={cn('places__option', { 'places__option--active': sortType === currentSortType })}
             tabIndex={0}
             key={sortType}
             onClick={(evt) => {
