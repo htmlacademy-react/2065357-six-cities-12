1a4325e3e3df385eed5fc73c2a1edfebb9ba89e1
Vanesflat <ptptcptptc@yandex.ru>
2023-04-11 20:43:34 +0700

    Major update store: token, login, authorization and add types

---
+++ project/src/store/user-slice/user-slice.ts
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { AuthorizationStatus } from '../../const';
+import { checkAuthAction, loginAction, logoutAction } from '../api-actions';
+
+type InitialState = {
+  authorizationStatus: AuthorizationStatus;
+  login: string;
+  avatarUrl: string;
+};
+
+const initialState: InitialState = {
+  authorizationStatus: AuthorizationStatus.Unknown,
+  login: '',
+  avatarUrl: ''
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(checkAuthAction.fulfilled, (state, action) => {
+        state.authorizationStatus = AuthorizationStatus.Auth;
+        state.login = action.payload.email;
+        state.avatarUrl = action.payload.avatarUrl;
+      })
+      .addCase(checkAuthAction.rejected, (state) => {
+        state.authorizationStatus = AuthorizationStatus.NoAuth;
+      })
+      .addCase(loginAction.fulfilled, (state, action) => {
+        state.authorizationStatus = AuthorizationStatus.Auth;
+        state.login = action.payload.email;
+        state.avatarUrl = action.payload.avatarUrl;
+      })
+      .addCase(loginAction.rejected, (state) => {
+        state.authorizationStatus = AuthorizationStatus.NoAuth;
+      })
+      .addCase(logoutAction.fulfilled, (state) => {
+        state.authorizationStatus = AuthorizationStatus.NoAuth;
+      });
+  }
+});
+
+export default userSlice.reducer;
