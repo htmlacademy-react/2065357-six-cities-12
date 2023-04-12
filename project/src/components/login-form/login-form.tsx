49ff2fc3bc0283227b02bf3ee703e2e0b320b51f
Vanesflat <ptptcptptc@yandex.ru>
2023-04-12 17:12:57 +0700

    Refactor validation

--- project/src/components/login-form/login-form.tsx
+++ project/src/components/login-form/login-form.tsx
@@ -3,46 +3,62 @@ import classes from './login-form.module.scss';
 import cn from 'classnames';
 import { useAppDispatch } from '../../hooks/use-app-dispatch/use-app-dispatch';
 import { loginAction } from '../../store/api-actions';
-import { AuthData } from '../../types/auth-data';
 
 const EMAIL_PATTERN = /^[_a-z0-9-+-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i;
 const PASSWORD_PATTERN = /(?=.*[a-z])(?=.*[0-9])/g;
 
-type Input = {
+const LOGIN_FIELDS: Record<string, string> = {
+  email: 'E-mail',
+  password: 'Password'
+};
+
+type Field = {
   value: string;
-  dirty: boolean;
+  error: boolean;
   pattern: RegExp;
   errorText: string;
 };
 
 function LoginForm(): JSX.Element {
-  const [email, setEmail] = useState<Input>({
-    value: '',
-    dirty: false,
-    pattern: EMAIL_PATTERN,
-    errorText: 'This mail is incorrect'
-  });
-
-  const [password, setPassword] = useState<Input>({
-    value: '',
-    dirty: false,
-    pattern: PASSWORD_PATTERN,
-    errorText: 'Password must contain at least one letter and number'
+  const [formData, setFormData] = useState<Record<string, Field>>({
+    email: {
+      value: '',
+      error: false,
+      pattern: EMAIL_PATTERN,
+      errorText: 'This mail is incorrect'
+    },
+    password: {
+      value: '',
+      error: false,
+      pattern: PASSWORD_PATTERN,
+      errorText: 'Password must contain at least one letter and number'
+    }
   });
 
   const dispatch = useAppDispatch();
 
-  const onSubmit = (authData: AuthData) => {
-    dispatch(loginAction(authData));
+  const isValid = Object.values(formData).every((field) => !field.error);
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = evt.target;
+
+    setFormData({
+      ...formData,
+      [name]: {
+        ...formData[name],
+        value: value,
+        error: !formData[name].pattern.test(value)
+      }
+    });
   };
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
-    onSubmit({
-      login: email.value,
-      password: password.value
-    });
+    dispatch(loginAction({
+      login: formData.email.value,
+      password: formData.password.value
+    }));
   };
 
   return (
@@ -52,47 +68,25 @@ function LoginForm(): JSX.Element {
       method="post"
       onSubmit={handleSubmit}
     >
-      <div className="login__input-wrapper form__input-wrapper">
-        <label className="visually-hidden">E-mail</label>
-        <input
-          className={cn('login__input form__input', email.dirty && classes.invalid)}
-          type="email"
-          name="email"
-          placeholder="Email"
-          onInput={(evt: ChangeEvent<HTMLInputElement>) => {
-            setEmail({
-              ...email,
-              value: evt.target.value,
-              dirty: !email.pattern.test(evt.target.value)
-            });
-          }}
-          required
-        />
-        {email.dirty && (<div className={classes.errorMessage}>{email.errorText}</div>)}
-      </div>
-      <div className="login__input-wrapper form__input-wrapper">
-        <label className="visually-hidden">Password</label>
-        <input
-          className={cn('login__input form__input', password.dirty && classes.invalid)}
-          type="password"
-          name="password"
-          placeholder="Password"
-          onInput={(evt: ChangeEvent<HTMLInputElement>) => {
-            setPassword({
-              ...password,
-              value: evt.target.value,
-              dirty: !password.pattern.test(evt.target.value)
-            });
-          }}
-          required
-        />
-        {password.dirty && (<div className={classes.errorMessage}>{password.errorText}</div>)}
-      </div>
+      {Object.entries(LOGIN_FIELDS).map(([name, label]) => (
+        <div className="login__input-wrapper form__input-wrapper" key={name}>
+          <label className="visually-hidden">{label}</label>
+          <input
+            className={cn('login__input form__input', formData[name].error && classes.invalid)}
+            type={name}
+            name={name}
+            placeholder={label}
+            onChange={handleChange}
+            required
+          />
+          {formData[name].error && (<div className={classes.errorMessage}>{formData[name].errorText}</div>)}
+        </div>
+      ))}
 
       <button
         className="login__submit form__submit button"
         type="submit"
-        disabled={email.dirty || password.dirty}
+        disabled={!isValid}
       >
         Sign in
       </button>
