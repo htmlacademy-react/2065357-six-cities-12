7a7e4e3b2fd040539185c72d04f400a568a1310f
Vanesflat <ptptcptptc@yandex.ru>
2023-04-14 17:30:54 +0700

    Add notificaton card component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,6 +5,7 @@ import App from './components/app/app';
 import { store } from './store/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import NotificationCard from './components/notification-card/notification-card';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,6 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ToastContainer />
+      <NotificationCard />
       <App />
     </Provider>
   </React.StrictMode>,
