9d5fc1f303c290ab6ff04922a1b5d15cad166e03
Vanesflat <ptptcptptc@yandex.ru>
2023-04-14 19:38:31 +0700

    Implement lazy load pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,54 +1,58 @@
+import { Suspense, lazy } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Routes, Route } from 'react-router-dom';
 import browserHistory from '../../browser-history';
 import { AppRoute } from '../../const';
 import { useAppSelector } from '../../hooks/use-app-selector/use-app-selector';
-import FavoritesPage from '../../pages/favorites-page/favorites-page';
-import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
-// import OfferPage from '../../pages/offer/offer';
-import Page404 from '../../pages/page-404/page-404';
 import { getAuthorizationStatus } from '../../store/user-slice/selectors';
 import HistoryRouter from '../history-router/history-router';
+import Loader from '../loader/loader';
 import PrivateRoute from '../private-route/private-route';
 
+const FavoritesPage = lazy(() => import('../../pages/favorites-page/favorites-page'));
+const LoginPage = lazy(() => import('../../pages/login-page/login-page'));
+// const OfferPage = lazy(() => import('../../pages/offer-page/offer-page'));
+const Page404 = lazy(() => import('../../pages/page-404/page-404'));
 
 function App(): JSX.Element {
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
 
   return (
-    <HelmetProvider>
-      <HistoryRouter history={browserHistory}>
-        <Routes>
-          <Route
-            path={AppRoute.Main}
-            element={<MainPage />}
-          />
-          <Route
-            path={AppRoute.Favorites}
-            element={
-              <PrivateRoute
-                authorizationStatus={authorizationStatus}
-              >
-                <FavoritesPage />
-              </PrivateRoute>
-            }
-          />
-          {/* <Route
+    <Suspense fallback={<Loader isSmall={false} />}>
+      <HelmetProvider>
+        <HistoryRouter history={browserHistory}>
+          <Routes>
+            <Route
+              path={AppRoute.Main}
+              element={<MainPage />}
+            />
+            <Route
+              path={AppRoute.Favorites}
+              element={
+                <PrivateRoute
+                  authorizationStatus={authorizationStatus}
+                >
+                  <FavoritesPage />
+                </PrivateRoute>
+              }
+            />
+            {/* <Route
             path={AppRoute.Offer}
             element={<OfferPage offers={offersByCity} comments={comments} />}
           /> */}
-          <Route
-            path='*'
-            element={<Page404 />}
-          />
-          <Route
-            path={AppRoute.Login}
-            element={<LoginPage />}
-          />
-        </Routes>
-      </HistoryRouter>
-    </HelmetProvider >
+            <Route
+              path='*'
+              element={<Page404 />}
+            />
+            <Route
+              path={AppRoute.Login}
+              element={<LoginPage />}
+            />
+          </Routes>
+        </HistoryRouter>
+      </HelmetProvider>
+    </Suspense>
   );
 }
 
