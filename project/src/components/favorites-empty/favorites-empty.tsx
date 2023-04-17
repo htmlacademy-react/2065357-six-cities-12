b5ad1b1c95dd38ec3b65da6718a08c80a28054f0
Vanesflat <ptptcptptc@yandex.ru>
2023-04-17 15:51:51 +0700

    Fix favorites empty component

--- project/src/components/favorites-empty/favorites-empty.tsx
+++ project/src/components/favorites-empty/favorites-empty.tsx
@@ -1,8 +1,12 @@
-import classes from './favorites-empty.module.scss';
-
 function FavoritesEmpty(): JSX.Element {
   return (
-    <p className={classes.text}>List is empty</p>
+    <section className="favorites favorites--empty">
+      <h1 className="visually-hidden">Favorites (empty)</h1>
+      <div className="favorites__status-wrapper">
+        <b className="favorites__status">Nothing yet saved.</b>
+        <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+      </div>
+    </section>
   );
 }
 
